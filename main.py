c42a85e81a8d2e25bb1eee86b164ff2dbe072027
Louis-He <1726110778@qq.com>
2017-10-15 12:59:10 -0400

    bug fixed

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
 import werobot
 import time
+#from apscheduler.schedulers.background import BackgroundScheduler
 
 robot = werobot.WeRoBot(token='louishe999617')
 
 @robot.handler
 def hello(message):
-    time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(message.time))
+    ts = '['+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(message.time))+']'
     print(message.source+' --> '+message.content)
     return 'Hello World!'
 
+'''
+def clearlog():
+    #clear logs every hour
+    f = open('/home/weather/hsefz_server/wxbot/record/txtrecord.txt', 'w')
+    f.write('[' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ']record重新写入')
+    f.close()  # you can omit in most cases as the destructor will call it
+    print('[' + time.strftime("%Y-%m-%d %H:%M:%S",
+                                time.localtime()) + ']record重制完成')
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(clearlog, 'interval', seconds = 3600 * 6)#间隔6小时执行一次
+scheduler.start()    #这里的调度任务是独立的一个线程
+'''
 # 让服务器监听在 0.0.0.0:80
 robot.config['HOST'] = '0.0.0.0'
 robot.config['PORT'] = 80
