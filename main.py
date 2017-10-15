aa553d7427f4f24f3ae790f62b2ea4af68ce5055
Louis-He <1726110778@qq.com>
2017-10-15 13:28:48 -0400

    bug fixed

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ scheduler.start()    #这里的调度任务是独立的一个线程
 @robot.handler
 def hello(msg):
     ts = '['+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(msg.time-4*60*60))+']'
-    f = open('/root/wechatbot/message.txt', 'w+')
+    f = open('/root/wechatbot/message.txt', 'a+')
     f.write(ts + '\t' + msg.source + ':\t' + msg.content)
     f.close()
     print(ts + msg.source+' --> '+msg.content)
